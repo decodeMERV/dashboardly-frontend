c7c7d369b4be3fb31b43b2f0edf01726da446a39
VinceLau <VinLau@users.noreply.github.com>
2017-08-08 19:12:46 -0400

    little progress

--- src/components/modals/editBoard.js
+++ src/components/modals/editBoard.js
@@ -6,13 +6,26 @@ import {withRouter} from 'react-router';
 
 
 class editBoard extends Component{
+
+  handleClickOutside = () => {
+    this.props.closeCreateBoard(); //This method should be defined in Home.js
+  }
+
+  _handleEditBoard = () => {
+
+  }
+
+
   render(){
     return(
       <div className={`editBoard `}>
-
+        <h2> Edit Board </h2>
+        <input type="text"/>
+        <input type="text"/>
+        <button onClick={this._handleEditBoard}>Process</button>
       </div>
     );
   }
 }
 
-export default withRouter(editBoard);
+export default withRouter(onClickOutside(editBoard));
